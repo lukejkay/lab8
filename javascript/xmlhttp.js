8ff29f08cbb66f350c52492a152ba036f740302a
Fania Ra <fania@web.de>
2017-11-20 21:50:56 +0000

    remove unnecessary code and rewrite readme

--- javascript/xmlhttp.js
+++ javascript/xmlhttp.js
@@ -10,28 +10,8 @@
   // get an api key from http://open-platform.theguardian.com/access/
   let baseURL = "https://content.guardianapis.com/search?api-key=<YOUR-API-KEY-HERE>&q=";
 
-  searchForm.addEventListener("submit", function(ev){
-    let url = baseURL + queryBox.value;
-    xhr.open("GET", url, true);
-    xhr.setRequestHeader('Api-User-Agent', 'Example/1.0');
-    xhr.send();
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      let response = JSON.parse(xhr.responseText);
-      gatherData(response);
-    }
-	  queryBox.value = "";
-      ev.preventDefault();
-    }, false);
-
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      let response = JSON.parse(xhr.responseText);
-      gatherData(response);
-    }
-  };
-
-  // GUARDIAN
   function gatherData(data) {
+    // console.log(data);
     let key;
     let theData = "";
     let tmp = data.response.results;
@@ -39,6 +19,21 @@
       theData += `<li><a href="${tmp[key].webUrl}">${tmp[key].webTitle}</a> ${tmp[key].webPublicationDate}</li>`;
     }
     demoJSON.innerHTML = theData;
-  }
+  };
+
+  searchForm.addEventListener("submit", function(ev){
+    let url = baseURL + queryBox.value;
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader('Api-User-Agent', 'Example/1.0');
+    xhr.send();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        let response = JSON.parse(xhr.responseText);
+        gatherData(response);
+      }
+    };
+	  queryBox.value = "";
+    ev.preventDefault();
+  }, false);
 
 }());
